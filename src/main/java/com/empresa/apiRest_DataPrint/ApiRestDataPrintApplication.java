a21200cd3829f7d42f6728d4d4f852a66bd422b4
Lenin <leninalbino@gmail.com>
2022-04-19 23:26:03 -0500

    [lenin] subiendo el primer proyecto del backend

---
+++ src/main/java/com/empresa/apiRest_DataPrint/ApiRestDataPrintApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.apiRest_DataPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDataPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDataPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/apiRest_DataPrint/ApiRestDataPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.apiRest_DataPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDataPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
