40f1a7f38bc0b38632337c8abe2cf27c8df14412
kirby239 <thumastercito428@gmail.com>
2022-06-10 00:11:34 -0500

    [Kirby] Poniento el CrossOrigin

--- src/main/java/com/empresa/apiRest_DataPrint/controller/UsuariosController.java
+++ src/main/java/com/empresa/apiRest_DataPrint/controller/UsuariosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.empresa.apiRest_DataPrint.model.Usuarios;
 import com.empresa.apiRest_DataPrint.service.UsuariosService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/rest/v1/usuarios")
 public class UsuariosController {
 
