38628187ad457a97ece4c53d75c74d665a0c51be
jhongarciamedina <jhonfarru.game@gmail.com>
2022-07-29 15:20:47 -0500

    [jhon]cambios en home

--- src/main/java/com/empresa/apiRest_DataPrint/controller/HomeController.java
+++ src/main/java/com/empresa/apiRest_DataPrint/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.empresa.apiRest_DataPrint.controller;
 
+import com.empresa.apiRest_DataPrint.model.Categorias;
 import com.empresa.apiRest_DataPrint.model.Producto;
+import com.empresa.apiRest_DataPrint.service.CategoriasService;
 import com.empresa.apiRest_DataPrint.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +18,24 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private ProductoService productoService;
+    
+    @Autowired
+    private CategoriasService categoriaService;
+    
 
+ //   @GetMapping("/")
+//    public ResponseEntity <List<Producto>> getCatalogue(){
+//        List<Producto> productsList = productoService.ListarProductos();
+//        return ResponseEntity.ok().body(productsList);
+//    }
+    
     @GetMapping("/")
-    public ResponseEntity <List<Producto>> getCatalogue(){
-        List<Producto> productsList = productoService.ListarProductos();
-        return ResponseEntity.ok().body(productsList);
-    }
+	public ResponseEntity<List<Producto>>ListFindAllProduct(){
+		return new ResponseEntity<List<Producto>>(productoService.ListarProductos(), HttpStatus.OK);
+		}
+    
+//    @GetMapping("/")
+//	public ResponseEntity<List<Categorias>> listarCategorias(){
+//		return new ResponseEntity<List<Categorias>>(categoriaService.listarCategorias(), HttpStatus.OK);
+//	}
 }
